5f6f7b5bb772e395cecb21ba951026402ee0e113
pullalarevusaipriya <saipriyapullalarevu344@gmail.com>
2021-11-11 17:14:43 +0530

    commitcomm

---
+++ com-cg-jpa-hibernate-lab-main/src/main/java/com/cg/jpa/hibernate/lab/App.java
@@ -0,0 +1,14 @@
+package com.cg.jpa.hibernate.lab;
+
+/**
+ * 
+ * @author pupriya
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
